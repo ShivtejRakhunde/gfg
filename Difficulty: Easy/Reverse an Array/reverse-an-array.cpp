872c5ada8ea8863c9915ba4972db66402f84d25c
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2024-11-17 22:37:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
@@ -8,7 +8,7 @@ class Solution {
   public:
     void reverseArray(vector<int> &arr) {
         // code here
-        reverse(arr.begin(),arr.end());
+        reverse(arr.begin(), arr.end());
     }
 };
 
