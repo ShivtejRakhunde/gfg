bcf37c8c2cc60f5d191781d61806c3bb16a95523
ShivtejRakhunde <146632550+ShivtejRakhunde@users.noreply.github.com>
2025-03-11 07:45:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Duplicates/array-duplicates.cpp
+++ Difficulty: Easy/Array Duplicates/array-duplicates.cpp
@@ -2,186 +2,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// #define endl "\n"
-// int mod = 1000ll * 1000ll * 1000ll + 7;
-// const int inf = 1000ll * 1000ll * 1000ll * 1000ll * 1000ll * 1000ll;
-// #define cout std::cout
-// #define cin std::cin
-
-// bool arrayEnded = false;
-
-// long long readInt(long long l, long long r, char endd) {
-//     long long x = 0;
-//     int cnt = 0;
-//     int fi = -1;
-//     bool is_neg = false;
-//     // integer must not be started from zero
-//     while (true) {
-//         char g = getchar();
-//         if (g == '-') {
-//             assert(fi == -1);
-//             is_neg = true;
-//             continue;
-//         }
-//         if ('0' <= g && g <= '9') {
-//             x *= 10;
-//             x += g - '0';
-//             if (cnt == 0) {
-//                 fi = g - '0';
-//             }
-//             cnt++;
-//             assert(fi != 0 || cnt == 1);
-//             assert(fi != 0 || is_neg == false);
-
-//             assert(!(cnt > 19 || (cnt == 19 && fi > 1)));
-//         } else if (g == endd || g == '\n' || g == -1) {
-//             if (is_neg) {
-//                 x = -x;
-//             }
-
-//             if (!(l <= x && x <= r)) {
-//                 cerr << l << ' ' << r << ' ' << x << '\n';
-//                 assert(1 == 0);
-//             }
-//             // -1  -> this is the last character i.e null
-//             if (g == '\n' or g == -1) { // if array ended
-//                 arrayEnded = true;
-//             }
-//             return x;
-//         } else {
-//             assert(false);
-//         }
-//     }
-// }
-
-// string readString(int l, int r, char endd) {
-//     string ret = "";
-//     int cnt = 0;
-//     while (true) {
-//         char g = getchar();
-//         assert(g != -1); // if invalid character
-//         if (g == endd) {
-//             break;
-//         }
-//         cnt++;
-//         ret += g;
-//     }
-//     assert(l <= cnt && cnt <= r);
-//     return ret;
-// }
-
-// long long readIntSp(long long l, long long r) {
-//     return readInt(l, r, ' ');
-// }
-
-// long long readIntLn(long long l, long long r) {
-//     return readInt(l, r, '\n');
-// }
-
-// string readStringLn(int l, int r) {
-//     return readString(l, r, '\n');
-// }
-
-// string readStringSp(int l, int r) {
-//     return readString(l, r, ' ');
-// }
-
-// vector<long long> readArray() {
-//     arrayEnded = false;
-//     int n = 0;
-//     vector<long long> arr;
-//     while (true) {
-//         n++;
-//         // change the array of elements accordingly
-//         arr.push_back(readInt(0, 100000, ' '));
-//         if (arrayEnded)
-//             break;
-//     }
-
-//     return arr;
-// }
-
-// int sumN = 0;
-
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    vector<int> duplicates(vector<int> arr) {
+    vector<int> findDuplicates(vector<int>& arr) {
         // code here
-        unordered_map<int, int> mp;  // Map to store frequency of elements
-    vector<int> ans;  // Vector to store duplicates
-
-    // Count the frequency of each element
-    for (int i = 0; i < arr.size(); i++) {
-        mp[arr[i]]++;
-    }
-
-    // Find the duplicates
-    for (auto it : mp) {
-        if (it.second > 1) {  // If an element occurs more than once
-            ans.push_back(it.first);
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        
+        for(int i=0; i<arr.size(); i++){
+            if(mp.find(arr[i])!=mp.end()){
+                ans.push_back(arr[i]);
+            }
+            else{
+                mp[arr[i]]++;
+            }
         }
-    }
-
-    // If no duplicates were found, return {-1}
-    if (ans.empty()) {
-        return {-1};
-    }
-    sort(ans.begin(), ans.end());
-    return ans;
+        return ans;
     }
 };
 
 
 //{ Driver Code Starts.
 
-void solve() {
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-
-    Solution obj;
-    vector<int> ans = obj.duplicates(arr);
-    for (int i : ans)
-        cout << i << ' ';
-    cout << endl;
-}
-
 int main() {
-    int t;
+    int t; // Number of test cases
     cin >> t;
-
+    cin.ignore(); // Ignore the newline after the integer input
     while (t--) {
-        solve();
+        vector<int> a;
+        string input;
+
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num); // Read the array elements from input string
+
+        Solution obj;
+        vector<int> result = obj.findDuplicates(a);
+        sort(result.begin(), result.end());
+
+        // Print the result in the required format
+        if (result.empty()) {
+            cout << "[]"; // Print empty brackets if no duplicates are found
+        } else {
+            for (int i = 0; i < result.size(); i++) {
+                if (i != 0)
+                    cout << " ";
+                cout << result[i];
+            }
+        }
+        cout << endl;
+        cout << "~" << endl;
     }
+
     return 0;
 }
 
-// int32_t main() {
-//     // #ifndef ONLINE_JUDGE
-//     //     freopen("input.txt", "r", stdin);
-//     //     freopen("output.txt", "w", stdout);
-//     // #endif
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL), cout.tie(NULL);
-//     int T = 1;
-//     T = readInt(1, 2000, '\n'); // if multiple test cases
-//     if (T == 1) {
-//         cout << -1 << endl;
-//         return 0;
-//     }
-//     cerr << "#Testcases read successfully" << endl;
-//     while (T--) {
-//         solve();
-//         // cout<<'\n';
-//     }
-//     cerr << sumN << '\n';
-//     assert(sumN <= 2000000);
-//     cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
-//     return 0;
-// }
 // } Driver Code Ends
